391cbe2efff6e6563da98a8363d1ed9ce5ef08ec
Giulia Mummolo <mail@giuliamummolo.com>
2020-07-05 22:34:41 +0100

    layout change

--- projects.js
+++ projects.js
@@ -1,14 +1,14 @@
+// let projectsBtn = document.querySelectorAll("button.project");
+// console.log(projectsBtn);
 
-let projectsBtn = document.querySelectorAll("button.project");
-console.log(projectsBtn);
 
-projectsBtn.forEach((el) =>
-  addEventListener("click", (e) => {
-    let id = e.target.id;
-    console.log(id);
+// projectsBtn.forEach((el) =>
+//   addEventListener("click", (e) => {
+//     let id = e.target.id;
+//     console.log(id);
 
-    window.location.replace(`./project-page.html?project=${id}`);
+//     window.location.replace(`./project-page.html?project=${id}`);
 
-    return;
-  })
-);
+//     return;
+//   })
+// );
